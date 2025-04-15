2f52fcc4ff6e7694268972174280ddae885f4d6f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-15 10:00:56 -0300

    att

--- build/build/amalgamation_build.lua
+++ build/build/amalgamation_build.lua
@@ -6,29 +6,10 @@ function amalgamation_build()
     alreay_amalamated_done = true
 
 
-    local runtime = darwin.camalgamator.generate_amalgamation("src/cli/main.c")
+    local runtime = darwin.camalgamator.generate_amalgamation("main.c")
     
   
-    darwin.dtw.write_file("release/"..PROJECT_NAME.." .c", runtime)
-
-    local api_one = darwin.camalgamator.generate_amalgamation("src/imports/imports.api_define.h")
-    darwin.dtw.write_file("release/"..PROJECT_NAME.."ApiOne.h", api_one)
-
-    local max_content = darwin.camalgamator.ONE_MB * 10
-    local max_recursion = 100
-
-    local dependencie_not_included = darwin.camalgamator.generate_amalgamation_with_callback(
-        "src/imports/imports.api_define.h",
-    function(import, path)
-        if darwin.dtw.ends_with(import,"dependencies.h") then
-            return "dont-include"
-        end
-        return "include-once"
-    end,
-    max_content,
-    max_recursion
-    )    
-    darwin.dtw.write_file("release/"..PROJECT_NAME.."ApiNoDependenciesIncluded.h ", dependencie_not_included)
+    darwin.dtw.write_file("release/"..PROJECT_NAME..".c ", runtime)
 
 
 end
