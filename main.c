e2385a7aeee6edfac53075f0e1a2e53f3965ab25
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-15 03:56:50 -0300

    att

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 
 #include "dependencies/CWebStudioOne.c"
 #include "dependencies/CArgvParseOne.c"
+
+//====================================FLAGS=========================================
+const int FLAGS_SIZE = 2;
+const char *PORTS_FLAGS[]={
+    "port",
+    "p"
+};
+
+const char *DYNAMIC_LIV_FLAGS[]={
+    "dynamic_lib",
+    "d"
+};
+
+const char *SINGLE_PROCESS_FLAGS[]={
+    "single_process",
+    "s"
+};
+
+const char *HELP_FLAGS[]={
+    "help",
+    "h"
+};
+
+
+//====================================NAMESPACE=========================================
 CwebNamespace cweb;
 CArgvParseNamespace argv_namespace;
+
+//====================================MAIN=========================================
 
 CwebHttpResponse *main_sever(CwebHttpRequest *request ){
 
@@ -14,7 +41,7 @@ int main(int argc, char *argv[]){
     argv_namespace = newCArgvParseNamespace();
     CArgvParse args = argv_namespace.newCArgvParse(argc,argv);
 
-    
+
 
     CwebServer server = newCwebSever(5000, main_sever);
     cweb.server.start(&server);
