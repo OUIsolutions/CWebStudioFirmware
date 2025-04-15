12efd4a1f5e5e24fc97ee7019fc1cd23c0e36395
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-15 13:14:45 -0300

    removing old installers

--- build/build_funcs.lua
+++ build/build_funcs.lua
@@ -2,12 +2,6 @@
 function create_build_funcs()
 return {
     amalgamation_build = amalgamation_build,
-    debian_static_build = debian_static_build,
-    rpm_static_build = rpm_static_build,
-    windowsi32_build = windowsi32_build,
-    windowsi64_build = windowsi64_build,
-    debian_deb_build = debian_deb_build,
-    local_build = local_build,
     zip_build=zip_build
 }
 end 
