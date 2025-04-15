a524189b922a6adc87bc67167fdb9ecf9794c46c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-15 09:52:08 -0300

    att

--- build/install_dependencies.lua
+++ build/install_dependencies.lua
@@ -3,9 +3,9 @@ function Install_dependencies()
     os.execute("mkdir -p dependencies")
 
     local libs = {
-        { url = "https://github.com/OUIsolutions/DoTheWorld/releases/download/10.0.1/doTheWorldOne.c", path = "dependencies/doTheWorld.c" },
-        { url = "https://github.com/OUIsolutions/C-argv-parser/releases/download/0.0.1/CArgvParseOne.c", path = "dependencies/CArgvParse.c" },
-        {url = "https://github.com/OUIsolutions/CWebStudio/releases/download/4.0.0/CWebStudioOne.c" , path = "dependencies/CWebStudio.c"},
+        { url = "https://github.com/OUIsolutions/DoTheWorld/releases/download/10.0.1/doTheWorldOne.c", path = "dependencies/doTheWorldOne.c" },
+        { url = "https://github.com/OUIsolutions/C-argv-parser/releases/download/0.0.1/CArgvParseOne.c", path = "dependencies/CArgvParseOne.c" },
+        {url = "https://github.com/OUIsolutions/CWebStudio/releases/download/4.0.0/CWebStudioOne.c" , path = "dependencies/CWebStudioOne.c"},
     }
     for _, lib in ipairs(libs) do
         local executor = function()
