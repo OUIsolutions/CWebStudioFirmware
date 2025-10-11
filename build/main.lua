15c7a86830079fd72cef022d098b23e6778c8385
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-11 03:30:29 -0300

    deps_adapt

--- build/main.lua
+++ build/main.lua
@@ -3,31 +3,8 @@ darwin = darwin
 
 
 function main()
-    local possible_test = darwin.argv.get_next_unused()
 
-    if possible_test == "test_container" then
-        local container_name = darwin.argv.get_next_unused()
-        if container_name == nil then
-            print("Please provide a container name")
-            return
-        end
-        os.execute("mkdir -p release")
-        local image = darwin.ship.create_machine(container_name .. ":latest")
-        image.provider = CONTANIZER
-
-        image.start({
-            flags = { "-it" },
-            volumes = {
-                { "./release", "/release" },
-            },
-        })
-
-        return
-    end
-    Install_dependencies()
     local build_funcs = create_build_funcs()
-
-
     darwin.dtw.remove_any("release")
     darwin.dtw.remove_any(".cache")
     local i = 1
